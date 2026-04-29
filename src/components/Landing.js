56e000034800312da673f932f114001638bf9d57
ShahzebKhan17 <786khanmohammad@gmail.com>
2026-04-30 00:44:34 +0530

    First commit-React news app

---
+++ src/components/Landing.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import '../styles/Landing.css';
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className="landing">
+        <div className="overlay">
+          <div className="content">
+            <h1>SPORTEWS</h1>
+            <p>All Sports. All News. All Here.</p>
+            <button className="btn">Explore News</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
